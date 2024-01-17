7a439b20850122a6a9452e7bce0016d23dba3ff0
Vladimir <vjankovic998@gmail.com>
2024-01-17 21:05:20 +0100

    Novo dodato Mobile version

--- main.js
+++ main.js
@@ -1,19 +1,45 @@
 document.addEventListener('DOMContentLoaded', function() {
-    // Get the elements by their IDs
-    const elementToHover = document.getElementById('single-technology-wrap');
-    const elementToChange = document.getElementById('slika-tehnologije');
-
-    // Add event listener for hover
-    elementToHover.addEventListener('mouseenter', function() {
-    // Add a class to the other element on hover
-        elementToChange.classList.add('animate__animated');
-        elementToChange.classList.add('animate__fadeIn');
+    var mobileMenuIcon = document.getElementById('mobileMenu');
+    var header = document.getElementById('header');
+    var footer = document.getElementById('footer');
+    var body = document.body;
+    mobileMenuIcon.addEventListener('click', function() {
+        if (!header.classList.contains('mobile-menu')) {
+            header.classList.add('mobile-menu');
+            footer.classList.add('mobile-menu');
+            body.classList.add('overflow');
+        } else {
+            header.classList.remove('mobile-menu');
+            footer.classList.remove('mobile-menu');
+            body.classList.remove('overflow');
+        }
     });
 
-    elementToHover.addEventListener('mouseleave', function() {
-        elementToChange.classList.remove('animate__animated');
-        elementToChange.classList.remove('animate__fadeIn');
-    });
-
-    // console.log(elementToHover);
+    var links = document.getElementsByClassName('main-menu-link');
+    for (i = 0; i < links.length; i++) {
+        links[i].addEventListener('click', function () {
+            header.classList.remove('mobile-menu');
+            footer.classList.remove('mobile-menu');
+            body.classList.remove('overflow');
+        })
+    }
 });
+
+var widthVersion = 'desktop';
+
+function removeMobileClasses() {
+    var windowWidth = window.innerWidth;
+
+    if (windowWidth < 992 && widthVersion === 'desktop') {
+        widthVersion = 'mobile';
+    } else if (windowWidth >= 992 && widthVersion === 'mobile') {
+        widthVersion = 'desktop';
+        document.getElementById('header').classList.remove('mobile-menu');
+        document.getElementById('footer').classList.remove('mobile-menu');
+        document.body.classList.remove('overflow');
+    }
+}
+
+removeMobileClasses();
+
+window.addEventListener("resize", removeMobileClasses);
